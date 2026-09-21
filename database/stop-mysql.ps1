1e53dbb1942e0d81136e41e7cde14ee7c57b898a
lunardelligabriel09-droid <lunardelli.gabriel09@gmail.com>
2026-09-21 11:06:04 +0000

    commit 8 -  terminando o stop- mysql, ultimas atualizações na pasta DATABASE - v.1.3

--- database/stop-mysql.ps1
+++ database/stop-mysql.ps1
@@ -5,4 +5,5 @@ $mysqladmin = "C:\Program Files\MySQL\MySQL Server 8.4\bin\mysqladmin.exe"
 $senha = Read-Host -Prompt "Senha do root do MySQL" -AsSecureString
 $senhaTexto = [Runtime.InteropServices.Marshal]::PtrToStringAuto([Runtime.InteropServices.Marshal]::SecureStringToBSTR($senha))
 
-
+& $mysqladmin -u root "-p$senhaTexto" shutdown
+Write-Output "Comando de desligamento enviado ao MySQL."
